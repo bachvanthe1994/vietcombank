96ade80c142e6991734f1e299e3c433d4eaf79f4
anhtt <anhtt@anhtt.officevnpay.com>
2019-11-25 11:46:17 +0700

    change to new design

---
+++ src/test/java/vnpay/vietcombank/flow/Template.java
@@ -0,0 +1,50 @@
+package vnpay.vietcombank.flow;
+
+import java.io.IOException;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import commons.Base;
+import commons.PageFactoryManager;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import pageObjects.LogInPageObject;
+
+public class Template extends Base {
+	AndroidDriver<AndroidElement> driver;
+	private LogInPageObject login;
+
+
+	@Parameters({ "deviceType", "deviceName", "deviceUDID", "hubURL", "appActivities", "appPackage", "appName" })
+	@BeforeClass
+	public void beforeClass(String deviceType, String deviceName, String udid, String url, String appActivities,
+			String appPackage, String appName) throws IOException, InterruptedException {
+		startServer();
+		log.info("Before class: Mo app ");
+		driver= openAndroidApp(deviceType, deviceName, udid, url, appActivities, appPackage, appName);
+
+		login = PageFactoryManager.getLoginPageObject(driver);
+		
+		
+
+	}
+
+	@Test
+	public void TC_01_KiemTraChonDiemDenKhiChuaChonDiemDi(){
+		System.out.println("Start");
+		
+	}
+	
+
+	
+
+	@AfterClass(alwaysRun = true)
+	public void afterClass() {
+//		closeApp();
+		service.stop();
+	}
+
+}
