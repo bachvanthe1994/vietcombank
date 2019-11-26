9e966f0ac57fc200c8098ddfb0d75844e25fd34b
hoangkm <hoangkm@vnpay.vn>
2019-11-26 13:52:53 +0700

    commit kiem tra popup tren man hinh dang ki dich vu MB qua IB

---
+++ src/test/java/vnpay/vietcombank/signin/SignIn_Data.java
@@ -0,0 +1,12 @@
+package vnpay.vietcombank.signin;
+
+public class SignIn_Data {
+	public class Message_popup {
+		public static final String MESSAGE_POPUP = "Số điện thoại chưa đăng ký hoặc đã ngừng dịch vụ. Quý khách vui lòng đăng ký dịch vụ tại quầy hoặc đăng ký bằng tài khoản VCB-iB@nking/VCB-Mobile Bankplus. Chi tiết liên hệ Hotline 24/7: 1900545413 để được trợ giúp.";
+		public static final String MESSAGE_SIGNIN_IB = "VCB-iB@nking";
+		public static final String MESSAGE_USED = "Tiếp tục sử dụng dịch vụ";
+		public static final String MESSAGE_OTP = "VCB-Smart OTP là phương thức xác thực giao dịch có tính bảo mật cao, đáp ứng yêu cầu của Ngân hàng Nhà nước đối với các giao dịch trên VCB-Mobile B@nking. Vui lòng nhấn Đăng ký ngay để có những trải nghiệm hoàn toàn khác biệt.";
+		public static final String MESSAGE_CANCEL  = "Hủy";
+		public static final String MESSAGE_NEXT  = "Tiếp tục";
+	}
+}
