3d7f1ea783e4c9de79decdbdc4654432b124a218
DucDM1 <minhducdo2603@gmail.com>
2019-11-27 18:32:04 +0700

    update Login Data

--- src/test/java/vietcombank_test_data/LogIn_Data.java
+++ src/test/java/vietcombank_test_data/LogIn_Data.java
@@ -22,7 +22,10 @@ public class LogIn_Data {
 		public static final String OTP_INSTRUCTION = "Quý khách vui lòng nhập OTP đã được gửi về số điện thoại";
 		public static final String EMPTY_MESSAGE = "Mật khẩu không được bỏ trống. Quý khách vui lòng kiểm tra lại";
 		public static final String WRONG_PASSWORD_MESSAGE = "Mật khẩu đăng nhập không chính xác. Quý khách lưu ý, chức năng kích hoạt dịch vụ trên thiết bị khác sẽ bị tạm khóa 24h nếu Quý khách nhập sai mật khẩu từ 5 lần trở lên";
-
+		public static final String PHONE_NUMBER_NOT_EMPTY = "Số điện thoại không được bỏ trống. Quý khách vui lòng kiểm tra lại";
+		public static final String PHONE_NUMBER_INVALID = "Số điện thoại không hợp lệ. Quý khách vui lòng kiểm tra lại.";
+		public static final String PHONE_NUMBER_VALID = "Tài khoản đã được kích hoạt trên thiết bị khác. Quý khách có đồng ý tiếp tục cài đặt ứng dụng Mobile B@nking trên thiết bị hiện tại không?";
+		public static final String PHONE_NOT_REGISTER = "Số điện thoại chưa đăng ký hoặc đã ngừng dịch vụ. Quý khách vui lòng đăng ký dịch vụ tại quầy hoặc đăng ký bằng tài khoản VCB-iB@nking/VCB-Mobile Bankplus. Chi tiết liên hệ Hotline 24/7: 1900545413 để được trợ giúp.";
 	}
 
 	public class UI {
