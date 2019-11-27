b228d92282be6c82dbce82d8ce3a8ec3bdcfca8f
DucDM1 <minhducdo2603@gmail.com>
2019-11-27 18:36:13 +0700

    update code and add login page UI

--- src/main/java/pageObjects/LogInPageObject.java
+++ src/main/java/pageObjects/LogInPageObject.java
@@ -1,10 +1,15 @@
 package pageObjects;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 
 import commons.AbstractPage;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import vietcombankUI.DynamicPageUIs;
+import vietcombankUI.LoginPageUIs;
 
 public class LogInPageObject extends AbstractPage{
 
