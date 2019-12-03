f951520f11c80063d7b53ca6d969400fb785bb07
DucDM1 <minhducdo2603@gmail.com>
2019-12-03 13:24:59 +0700

    add test script TransferMoneyOutSideVCB

---
+++ src/test/java/vnpay/vietcombank/transfer_money_out_side_vbc/TransferMoneyOutSideVCB.java
@@ -0,0 +1,63 @@
+package vnpay.vietcombank.transfer_money_out_side_vbc;
+
+import java.io.IOException;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import commons.Base;
+import commons.PageFactoryManager;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import pageObjects.LogInPageObject;
+import vietcombank_test_data.LogIn_Data;
+
+public class TransferMoneyOutSideVCB extends Base {
+	AndroidDriver<AndroidElement> driver;
+	private LogInPageObject login;
+
+
+	@Parameters({ "deviceType", "deviceName", "deviceUDID", "hubURL", "appActivities", "appPackage", "appName" })
+	@BeforeClass
+	public void beforeClass(String deviceType, String deviceName, String udid, String url, String appActivities,
+			String appPackage, String appName) throws IOException, InterruptedException {
+		startServer();
+		log.info("Before class: Mo app ");
+		driver= openAndroidApp(deviceType, deviceName, udid, url, appActivities, appPackage, appName);
+
+		login = PageFactoryManager.getLoginPageObject(driver);
+		
+		login.clickToDynamicAcceptButton(driver, "com.android.packageinstaller:id/permission_allow_button");
+		
+		login.inputToDynamicLogInTextBox(driver, LogIn_Data.Login_Account.PHONE, "Tiếp tục");
+		
+		login.clickToDynamicButton(driver, "Tiếp tục");
+		
+		login.inputToDynamicInputBox(driver, LogIn_Data.Login_Account.NEW_PASSWORD, LogIn_Data.UI.PASSWORD_LABEL);
+		
+		login.clickToDynamicButton(driver, "Tiếp tục");
+		
+		login.inputToDynamicOtpOrPIN(driver, LogIn_Data.Login_Account.OTP, "Tiếp tục");
+		
+		login.clickToDynamicButton(driver, "Tiếp tục");
+		
+		login.clickToDynamicButton(driver, "Hủy");
+	}
+
+	@Test
+	public void TC_01_KiemTraManHinhDangNhap(){
+		System.out.println("Start");
+			
+		log.info("TC_01_1_Click Chuyen tien toi ngan hang khac");
+		login.clickToDynamicButionLinkOrLinkText(driver, "Chuyển tiền tới ngân hàng khác");
+	}
+	
+	@AfterClass(alwaysRun = true)
+	public void afterClass() {
+//		closeApp();
+		service.stop();
+	}
+
+}
