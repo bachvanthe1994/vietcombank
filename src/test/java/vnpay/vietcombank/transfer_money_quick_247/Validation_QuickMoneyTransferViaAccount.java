7465c4b81ffa304fd014613e26e9e25f86e3a87c
hiepit08 <hieppt@vnpay.vn>
2019-12-18 14:26:45 +0700

    Delete file tranferMoney_Data and add file LuckyGift and validation quick 24/7

---
+++ src/test/java/vnpay/vietcombank/transfer_money_quick_247/Validation_QuickMoneyTransferViaAccount.java
@@ -0,0 +1,96 @@
+package vnpay.vietcombank.transfer_money_quick_247;
+
+import java.io.IOException;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import commons.Base;
+import commons.PageFactoryManager;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import pageObjects.HomePageObject;
+import pageObjects.LogInPageObject;
+import pageObjects.TransactionReportPageObject;
+import pageObjects.TransferMoneyObject;
+import vietcombank_test_data.LogIn_Data;
+import vietcombank_test_data.TransferMoneyQuick_Data;
+import vietcombank_test_data.TransferMoney_Data;
+
+public class Validation_QuickMoneyTransferViaAccount extends Base {
+	AndroidDriver<AndroidElement> driver;
+	private TransactionReportPageObject transReport;
+	private String transferTime;
+	private LogInPageObject login;
+	private HomePageObject homePage;
+	private TransferMoneyObject transferMoney;
+	private String transactionNumber;
+	private String amountStartString;
+	private long amountStart;
+	private String amountTranferString;
+	private long amountTranfer;
+	private String costTranferString;
+	private long costTranfer;
+
+	@Parameters({ "deviceType", "deviceName", "deviceUDID", "hubURL", "appActivities", "appPackage", "appName" })
+	@BeforeClass
+	public void beforeClass(String deviceType, String deviceName, String udid, String url, String appActivities,
+			String appPackage, String appName) throws IOException, InterruptedException {
+		startServer();
+		log.info("Before class: Mo app ");
+		driver = openAndroidApp(deviceType, deviceName, udid, url, appActivities, appPackage, appName);
+
+		login = PageFactoryManager.getLoginPageObject(driver);
+
+		log.info("Before class: Click Allow Button");
+		login.clickToDynamicAcceptButton(driver, "com.android.packageinstaller:id/permission_allow_button");
+		log.info("Before class");
+		login.inputToDynamicLogInTextBox(driver, LogIn_Data.Login_Account.PHONE, "Tiếp tục");
+
+		log.info("Before class");
+		login.clickToDynamicButton(driver, "Tiếp tục");
+
+		log.info("Before class");
+		login.inputToDynamicInputBox(driver, LogIn_Data.Login_Account.NEW_PASSWORD, LogIn_Data.UI.PASSWORD_LABEL);
+
+		log.info("Before class");
+		login.clickToDynamicButton(driver, "Tiếp tục");
+
+		log.info("Before class");
+		login.inputToDynamicOtpOrPIN(driver, LogIn_Data.Login_Account.OTP, "Tiếp tục");
+
+		log.info("Before class");
+		login.clickToDynamicButton(driver, "Tiếp tục");
+		
+		log.info("Before class");
+		login.clickToDynamicButton(driver, "CHO PHÉP");
+
+		transferMoney = PageFactoryManager.getTransferMoneyObject(driver);
+	}
+
+	@Test
+	public void TC_01_ValidationChuyenNhanhQuaTaiKhoan() {
+		log.info("TC_01_Step_Scoll den man hinh chuyen tien nhanh");
+		transferMoney.scrollToText(driver, "Chuyển tiền tới ngân hàng khác");
+
+		log.info("TC_01_Step_Click Chuyen tien nhanh");
+		transferMoney.clickToDynamicButtonLinkOrLinkText(driver, "Chuyển tiền nhanh 24/7");
+
+		log.info("TC_01_Step_Kiem tra button back");
+		transferMoney.clickToDynamicBottomMenu(driver, 	"com.VCB:id/ivTitleLeft");
+		
+		log.info("TC_01_Step_Quay lai man hinh chuyen tien nhanh");
+		transferMoney.scrollToText(driver, "Chuyển tiền tới ngân hàng khác");
+
+		log.info("TC_01_Step_Click Chuyen tien nhanh");
+		transferMoney.clickToDynamicButtonLinkOrLinkText(driver, "Chuyển tiền nhanh 24/7");
+		
+		log.info("TC_01_Step_Check value trong hinh thuc chuyen tien");
+		transferMoney.clickToDynamicButtonLinkOrLinkText(driver, TransferMoneyQuick_Data.TransferQuick.OPTION_TRANSFER[0]);
+		transferMoney.isDynamicMessageAndLabelTextDisplayed(driver, TransferMoneyQuick_Data.TransferQuick.OPTION_TRANSFER[0]);
+		transferMoney.isDynamicMessageAndLabelTextDisplayed(driver, TransferMoneyQuick_Data.TransferQuick.OPTION_TRANSFER[1]);
+	}
+
+	
+}
