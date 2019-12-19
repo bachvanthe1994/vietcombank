125d8f1d6f44d932f17e60c857d9893f16c5dd9c
Ta Thi Anh <tathianh.3k11@gmail.com>
2019-12-19 19:36:41 +0700

    change test

--- src/test/java/vnpay/vietcombank/transfer_money_in_vcb/test.java
+++ src/test/java/vnpay/vietcombank/transfer_money_in_vcb/test.java
@@ -1,17 +1,33 @@
 package vnpay.vietcombank.transfer_money_in_vcb;
 
-public class test {
+import java.text.DecimalFormat;
+
+import com.fasterxml.jackson.databind.deser.Deserializers.Base;
+
+public class test extends Base {
 
 	public static void main(String[] args) {
-		String money = "4,317,842,003 VND";
-		 long m = convertMoneytoInt(money);
-		System.out.println(m);
-
+		String text ="1 USD ~ 30,000 VND";
+		String [] exchangeRateUSD =text.split(" ~ ");
+		System.out.println(exchangeRateUSD[1]);
+		long rate = convertMoneyToLong(exchangeRateUSD[1], "VND");
+		System.out.println(rate);
+	    
 	}
-public static long convertMoneytoInt(String money) {
-	  money = money.replaceAll(" VND", "");
-	  money = money.replaceAll(",", "");
-	  long m = Long.parseLong(money);
-	  return m;
-}
+
+	
+	public static long convertMoneyToLong(String money, String currency) {
+		money = money.replaceAll(" " + currency, "");
+		money = money.replaceAll(",", "");
+		long m = Long.parseLong(money);
+		return m;
+	}
+	
+	
+	
+	
+	
+	
+	
+
 }
