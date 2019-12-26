b8f0f8a5f88814559c0df3a0601635777cafdcab
DucDM1 <57981967+DucDM1@users.noreply.github.com>
2019-12-26 10:53:26 +0700

    complete fix bug merge

--- src/main/java/vietcombankUI/TransferMoneyCharityPageUIs.java
+++ src/main/java/vietcombankUI/TransferMoneyCharityPageUIs.java
@@ -4,4 +4,5 @@ public class TransferMoneyCharityPageUIs {
 	public static final String LIST_MONEY_SUGGESTION = "//android.widget.TextView";
 	public static final String LIST_MONEY_SUGGESTION_ID = "com.VCB:id/rvSuggest";
 	public static final String TEXT_VIEW_CURRENCY_CHARITY = "//android.widget.EditText['Số tiền ủng hộ']/following-sibling::android.widget.TextView";
+	public static final String TEXT_VIEW_LIST_ORGANIZATION = "//android.widget.ListView//android.widget.TextView";
 }
