658a81032f176490a42abe9a207acc771d68d674
AnhTaQA <tathianh.3k11@gmail.com>
2020-01-14 09:37:03 +0700

    run tc

--- src/test/java/vnpay/vietcombank/transfer_money_in_vcb/Transfer_Money_Immedidately_Validation_Part1.java
+++ src/test/java/vnpay/vietcombank/transfer_money_in_vcb/Transfer_Money_Immedidately_Validation_Part1.java
@@ -65,7 +65,7 @@ public class Transfer_Money_Immedidately_Validation_Part1 extends Base {
 	}
 
 	@Test
-	public void TC_01_KiemTraTaiKhoanMacDinh() {
+	public void TC_01_KiemTraTaiKhoanNguonMacDinh() {
 
 		String defaultAccount = homePage.getDynamicTextDetailByID(driver, "com.VCB:id/tvDefaultAcc");
 		log.info("TC_01_Step_01: Click Chuyen tien trong VCB");
