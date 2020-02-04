bb81167211632eb43818f56b2a12367490c9e333
DucDM1 <57981967+DucDM1@users.noreply.github.com>
2020-02-04 16:18:24 +0700

    refactor code

--- src/main/java/pageObjects/sdk/hotelBooking/HotelBookingPageObject.java
+++ src/main/java/pageObjects/sdk/hotelBooking/HotelBookingPageObject.java
@@ -1,8 +1,6 @@
 package pageObjects.sdk.hotelBooking;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.openqa.selenium.By;
