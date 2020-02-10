0d191fdbec8ba609413935b9451e467041c633ee
AnhTaQA <tathianh.3k11@gmail.com>
2020-02-10 17:14:50 +0700

    fix code

--- src/test/java/vnpay/vietcombank/sdk_train_ticket/Validation_TrainTicket_2.java
+++ src/test/java/vnpay/vietcombank/sdk_train_ticket/Validation_TrainTicket_2.java
@@ -1,4 +1,4 @@
-package vnpay.vietcombank.SDK_train_ticket;
+package vnpay.vietcombank.sdk_train_ticket;
 
 import java.io.IOException;
 import java.util.List;
@@ -48,37 +48,37 @@ public class Validation_TrainTicket_2 extends Base {
 
 		log.info("TC_06_Click ga khoi hanh");
 		trainTicket.clickDynamicPointStartAndEnd(driver, "ĐẶT VÉ TÀU", TrainTicket_Data.textDefault.TITLE_START);
-		
+
 		log.info("TC_06_Nhap text ga khoi hanh");
 		trainTicket.inputToDynamicTextPoint(driver, TrainTicket_Data.inputText.POINT_START_END_VALID, "0");
-		
+
 		log.info("TC_06_Chon gia tri trong danh sach");
 		trainTicket.clickToDynamicButtonLinkOrLinkText(driver, TrainTicket_Data.inputText.POINT_START_END_VALID);
-		
+
 		log.info("TC_06_Nhap text ga den");
 		trainTicket.inputToDynamicTextPoint(driver, TrainTicket_Data.inputText.POINT_EDIT_SEARCH, "2");
-		
+
 		log.info("TC_06_Chon gia tri trong danh sach");
 		trainTicket.clickToDynamicButtonLinkOrLinkText(driver, TrainTicket_Data.inputText.POINT_EDIT_SEARCH);
-		
+
 		log.info("TC_01_Check text ga khoi hanh man hinh Dat ve tau truoc khi doi ga");
-		verifyEquals(trainTicket.getDynamicTextPointStart(driver, "com.VCB:id/tvTextPickUp"),  TrainTicket_Data.inputText.POINT_START_END_VALID);
+		verifyEquals(trainTicket.getDynamicTextPointStart(driver, "com.VCB:id/tvTextPickUp"), TrainTicket_Data.inputText.POINT_START_END_VALID);
 
 		log.info("TC_01_Check text ga den man hinh dat ve tau truoc khi doi ga");
-		verifyEquals(trainTicket.getDynamicTextPointStart(driver, "com.VCB:id/tvTextArrival"),  TrainTicket_Data.inputText.POINT_EDIT_SEARCH);
-		
+		verifyEquals(trainTicket.getDynamicTextPointStart(driver, "com.VCB:id/tvTextArrival"), TrainTicket_Data.inputText.POINT_EDIT_SEARCH);
+
 		log.info("TC_01_Click button change");
 		trainTicket.clickToDynamicIconChange(driver, TrainTicket_Data.inputText.POINT_EDIT_SEARCH);
-		
+
 		log.info("TC_01_Check text ga khoi hanh man hinh Dat ve tau sau khi doi ga");
-		verifyEquals(trainTicket.getDynamicTextPointStart(driver, "com.VCB:id/tvTextPickUp"),  TrainTicket_Data.inputText.POINT_EDIT_SEARCH);
+		verifyEquals(trainTicket.getDynamicTextPointStart(driver, "com.VCB:id/tvTextPickUp"), TrainTicket_Data.inputText.POINT_EDIT_SEARCH);
 
 		log.info("TC_01_Check text ga den man hinh dat ve tau sau khi doi ga");
-		verifyEquals(trainTicket.getDynamicTextPointStart(driver, "com.VCB:id/tvTextArrival"),  TrainTicket_Data.inputText.POINT_START_END_VALID);
+		verifyEquals(trainTicket.getDynamicTextPointStart(driver, "com.VCB:id/tvTextArrival"), TrainTicket_Data.inputText.POINT_START_END_VALID);
 	}
-	
+
 	@Test
 	public void TC_01_CheckHienThiThoiGianKhoiHanh() {
-	
+
 	}
 }
