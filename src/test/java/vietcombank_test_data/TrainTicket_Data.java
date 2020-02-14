cfb1dbe371b46717afc25bd54dd282905530c47a
hiepit08 <hieppt@vnpay.vn>
2020-02-14 17:39:39 +0700

    edit code validation

--- src/test/java/vietcombank_test_data/TrainTicket_Data.java
+++ src/test/java/vietcombank_test_data/TrainTicket_Data.java
@@ -25,7 +25,7 @@ public class TrainTicket_Data {
 		public static final String POINT_START_END_VALID ="Biên Hòa";
 		public static final String POINT_EDIT_SEARCH ="Hà Nội";
 		public static final String POINT_EDIT_SEARCH_END ="Vinh";
-		public static final String POINT_SERACH_THE_SAME ="Hải";
+		public static final String POINT_SERACH_THE_SAME ="hai";
 	
 	}
 	}
