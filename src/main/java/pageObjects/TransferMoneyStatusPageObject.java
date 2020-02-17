f77afd2b906c28f21d5e8dad884b6b66237bd91e
AnhTaQA <tathianh.3k11@gmail.com>
2020-02-17 10:32:07 +0700

    remove unused method

--- src/main/java/pageObjects/TransferMoneyStatusPageObject.java
+++ src/main/java/pageObjects/TransferMoneyStatusPageObject.java
@@ -3,23 +3,13 @@ package pageObjects;
 import commons.AbstractPage;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
-import vietcombankUI.TransferStatusPageUIs;
 
 public class TransferMoneyStatusPageObject extends AbstractPage {
 
-    public TransferMoneyStatusPageObject(AndroidDriver<AndroidElement> mappingDriver) {
-	driver = mappingDriver;
-    }
+	public TransferMoneyStatusPageObject(AndroidDriver<AndroidElement> mappingDriver) {
+		driver = mappingDriver;
+	}
 
-    private AndroidDriver<AndroidElement> driver;
+	private AndroidDriver<AndroidElement> driver;
 
-    public String getDynamicTextTitle(AndroidDriver<AndroidElement> driver, String dynamicID) {
-	waitForElementVisible(driver, TransferStatusPageUIs.DYNAMIC_TITLE, dynamicID);
-	return getTextElement(driver, TransferStatusPageUIs.DYNAMIC_TITLE, dynamicID);
-    }
-
-    public void clickToDynamictTextBox(AndroidDriver<AndroidElement> driver, String dynamicID) {
-	waitForElementVisible(driver, TransferStatusPageUIs.DYNAMIC_TRANSFER, dynamicID);
-	clickToElement(driver, TransferStatusPageUIs.DYNAMIC_TRANSFER, dynamicID);
-    }
 }
