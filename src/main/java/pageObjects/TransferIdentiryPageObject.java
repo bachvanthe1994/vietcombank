fc49edcbaaa73556c8be1306d808e1894f1da119
AnhTaQA <tathianh.3k11@gmail.com>
2020-02-28 11:58:47 +0700

    fix method with new page object

--- src/main/java/pageObjects/TransferIdentiryPageObject.java
+++ src/main/java/pageObjects/TransferIdentiryPageObject.java
@@ -3,80 +3,103 @@ package pageObjects;
 import commons.AbstractPage;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
-import vietcombankUI.DynamicPageUIs;
 import vietcombankUI.TransferIdentityPageUIs;
 
 public class TransferIdentiryPageObject extends AbstractPage {
 
-    public TransferIdentiryPageObject(AndroidDriver<AndroidElement> mappingDriver) {
-	driver = mappingDriver;
-    }
+	public TransferIdentiryPageObject(AndroidDriver<AndroidElement> mappingDriver) {
+		driver = mappingDriver;
+	}
 
-    private AndroidDriver<AndroidElement> driver;
+	private AndroidDriver<AndroidElement> driver;
 
-    public String getDynamicAmountLabelConvertVNDToLong(AndroidDriver<AndroidElement> driver, String dynamicTextValue) {
-	String moneyString = getDynamicAmountLabel(driver, dynamicTextValue);
-	moneyString = moneyString.replaceAll("Phí: ", "");
-	moneyString = moneyString.replaceAll(",", "");
-	moneyString = moneyString.replaceAll("VND/giao dịch", "");
-	return moneyString;
+	public String getDynamicAmountLabelConvertVNDToLong(AndroidDriver<AndroidElement> driver, String dynamicTextValue) {
+		String moneyString = getDynamicAmountLabel(driver, dynamicTextValue);
+		moneyString = moneyString.replaceAll("Phí: ", "");
+		moneyString = moneyString.replaceAll(",", "");
+		moneyString = moneyString.replaceAll("VND/giao dịch", "");
+		return moneyString;
 
-    }
+	}
 
-    public String getTextInDynamicIdentifition(AndroidDriver<AndroidElement> driver, String... dynamicIndex1Index2) {
-	waitForElementVisible(driver, TransferIdentityPageUIs.DYNAMIC_IDENTIFITION, dynamicIndex1Index2);
-	return getTextElement(driver, TransferIdentityPageUIs.DYNAMIC_IDENTIFITION, dynamicIndex1Index2);
+	public String getTextInDynamicIdentifition(AndroidDriver<AndroidElement> driver, String... dynamicIndex1Index2) {
+		boolean status = false;
+		String text = null;
+		status = waitForElementVisible(driver, TransferIdentityPageUIs.DYNAMIC_IDENTIFITION, dynamicIndex1Index2);
 
-    }
+		if (status == true) {
+			text = getTextElement(driver, TransferIdentityPageUIs.DYNAMIC_IDENTIFITION, dynamicIndex1Index2);
 
-    public String getTextDynamicPopupTransfers(AndroidDriver<AndroidElement> driver, String dynamicTextValue) {
-	waitForElementVisible(driver, DynamicPageUIs.DYNAMIC_BUTTON_LINK_LABEL_TEXT, dynamicTextValue);
-	return getTextElement(driver, DynamicPageUIs.DYNAMIC_BUTTON_LINK_LABEL_TEXT, dynamicTextValue);
+		}
+		return text;
 
-    }
+	}
 
-    public void inputToDynamicInputBoxIdentityValidate(AndroidDriver<AndroidElement> driver, String inputValue, String dynamicTextValue) {
-	scrollToText(driver, dynamicTextValue);
-	waitForElementVisible(driver, DynamicPageUIs.DYNAMIC_INPUT_BOX, dynamicTextValue);
-	sendKeyToElement(driver, DynamicPageUIs.DYNAMIC_INPUT_BOX, inputValue, dynamicTextValue);
+	public String getDynamicPopupPassword(AndroidDriver<AndroidElement> driver, String dynamicTextValue) {
+		boolean status = false;
+		String text = null;
+		status = waitForElementVisible(driver, TransferIdentityPageUIs.DYNAMIC_PASSWORD, dynamicTextValue);
 
-    }
+		if (status == true) {
+			text = getTextElement(driver, TransferIdentityPageUIs.DYNAMIC_PASSWORD, dynamicTextValue);
 
-    public String getDynamicTextHeader(AndroidDriver<AndroidElement> driver) {
-	waitForElementVisible(driver, TransferIdentityPageUIs.DYNAMIC_TEXT_HEADER);
-	return getTextElement(driver, TransferIdentityPageUIs.DYNAMIC_TEXT_HEADER);
-    }
+		}
+		return text;
+	}
 
-    public String getDynamicPopupPassword(AndroidDriver<AndroidElement> driver, String dynamicTextValue) {
-	waitForElementVisible(driver, TransferIdentityPageUIs.DYNAMIC_PASSWORD, dynamicTextValue);
-	return getTextElement(driver, TransferIdentityPageUIs.DYNAMIC_PASSWORD, dynamicTextValue);
-    }
+	public void clickToDynamicHomeIcon(AndroidDriver<AndroidElement> driver, String dynamicTextValue) {
+		boolean status = false;
+		scrollIDown(driver, TransferIdentityPageUIs.DYNAMIC_HOME_ICON, dynamicTextValue);
+		status = waitForElementVisible(driver, TransferIdentityPageUIs.DYNAMIC_HOME_ICON, dynamicTextValue);
+		if (status == true) {
+			clickToElement(driver, TransferIdentityPageUIs.DYNAMIC_HOME_ICON, dynamicTextValue);
 
-    public void clickToDynamicHomeIcon(AndroidDriver<AndroidElement> driver, String dynamicTextValue) {
-	scrollToText(driver, dynamicTextValue);
-	waitForElementVisible(driver, TransferIdentityPageUIs.DYNAMIC_HOME_ICON, dynamicTextValue);
-	clickToElement(driver, TransferIdentityPageUIs.DYNAMIC_HOME_ICON, dynamicTextValue);
+		}
 
-    }
+	}
 
-    public String getDynamicTextTitle(AndroidDriver<AndroidElement> driver, String dynamicTextValue) {
-	waitForElementVisible(driver, TransferIdentityPageUIs.DYNAMIC_TITLE_CONFIRM_TRANSFER, dynamicTextValue);
-	return getTextElement(driver, TransferIdentityPageUIs.DYNAMIC_TITLE_CONFIRM_TRANSFER, dynamicTextValue);
-    }
+	public String getDynamicTextTitle(AndroidDriver<AndroidElement> driver, String dynamicTextValue) {
+		boolean status = false;
+		String text = null;
+		status = waitForElementVisible(driver, TransferIdentityPageUIs.DYNAMIC_TITLE_CONFIRM_TRANSFER, dynamicTextValue);
+		if (status == true) {
+			text = getTextElement(driver, TransferIdentityPageUIs.DYNAMIC_TITLE_CONFIRM_TRANSFER, dynamicTextValue);
 
-    public String getDynamicTextTitleMoneyUSD(AndroidDriver<AndroidElement> driver, String dynamicTextValue) {
-	waitForElementVisible(driver, TransferIdentityPageUIs.DYNAMIC_MONEY_USD, dynamicTextValue);
-	return getTextElement(driver, TransferIdentityPageUIs.DYNAMIC_MONEY_USD, dynamicTextValue);
-    }
+		}
+		return text;
+	}
 
-    public String getDynamicAccountTitle(AndroidDriver<AndroidElement> driver, String dynamicIdValue) {
-	waitForElementVisible(driver, TransferIdentityPageUIs.DYNAMIC_ACCOUNT_CONFIRM, dynamicIdValue);
-	return getTextElement(driver, TransferIdentityPageUIs.DYNAMIC_ACCOUNT_CONFIRM, dynamicIdValue);
-    }
+	public String getDynamicTextTitleMoneyUSD(AndroidDriver<AndroidElement> driver, String dynamicTextValue) {
+		boolean status = false;
+		String text = null;
+		status = waitForElementVisible(driver, TransferIdentityPageUIs.DYNAMIC_MONEY_USD, dynamicTextValue);
+		if (status == true) {
+			text = getTextElement(driver, TransferIdentityPageUIs.DYNAMIC_MONEY_USD, dynamicTextValue);
 
-    public String getTextConfirmOtp(AndroidDriver<AndroidElement> driver, String dynamicTextValue) {
-	waitForElementVisible(driver, TransferIdentityPageUIs.DYNAMIC_OTP_CONFIRM, dynamicTextValue);
-	return getTextElement(driver, TransferIdentityPageUIs.DYNAMIC_OTP_CONFIRM, dynamicTextValue);
-    }
+		}
+		return text;
+	}
+
+	public String getDynamicAccountTitle(AndroidDriver<AndroidElement> driver, String dynamicIdValue) {
+		boolean status = false;
+		String text = null;
+		status = waitForElementVisible(driver, TransferIdentityPageUIs.DYNAMIC_ACCOUNT_CONFIRM, dynamicIdValue);
+		if (status == true) {
+			text = getTextElement(driver, TransferIdentityPageUIs.DYNAMIC_ACCOUNT_CONFIRM, dynamicIdValue);
+
+		}
+		return text;
+	}
+
+	public String getTextConfirmOtp(AndroidDriver<AndroidElement> driver, String dynamicTextValue) {
+		boolean status = false;
+		String text = null;
+		status = waitForElementVisible(driver, TransferIdentityPageUIs.DYNAMIC_OTP_CONFIRM, dynamicTextValue);
+		if (status == true) {
+			text = getTextElement(driver, TransferIdentityPageUIs.DYNAMIC_OTP_CONFIRM, dynamicTextValue);
+
+		}
+		return text;
+	}
 
 }
