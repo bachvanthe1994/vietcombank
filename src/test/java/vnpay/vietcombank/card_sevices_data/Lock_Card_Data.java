e47f95368b0a1c598a7f43661f170777c191e6fc
TrungND <trungnd@vnpay.vn>
2020-03-04 15:43:12 +0700

    TrungND - Update TC Goi 4

--- src/test/java/vnpay/vietcombank/card_sevices_data/Lock_Card_Data.java
+++ src/test/java/vnpay/vietcombank/card_sevices_data/Lock_Card_Data.java
@@ -2,6 +2,9 @@ package vnpay.vietcombank.card_sevices_data;
 
 public class Lock_Card_Data {
 	
+	public static final String DEBIT_CARD01 = "970436...084";
+	public static final String CREDIT_CARD01 = "356770...112";
+	public static final String CREDIT_CARD02 = "625124...350";
 	}
 	
 
