364a481e1579020d28beb46fe4cdfe22cce0fa78
DucDM1 <57981967+DucDM1@users.noreply.github.com>
2020-03-07 09:14:05 +0700

    replace SavingOnline_Data file

--- src/test/java/vietcombank_test_data/SavingOnline_Data.java
+++ src/test/java/vietcombank_test_data/SavingOnline_Data.java
@@ -1,4 +1,4 @@
-package vnpay.vietcombank.saving_online.data;
+package vietcombank_test_data;
 
 public class SavingOnline_Data {
 	public static final String SUCCESS_TRANSACTION = "GIAO DỊCH THÀNH CÔNG";
