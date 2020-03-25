e3c88b560070df863b004eaae32c77e212786084
AnhTaQA <tathianh.3k11@gmail.com>
2020-03-25 10:38:19 +0700

    add template

--- src/test/java/vnpay/vietcombank/login/testwwebview.java
+++ src/test/java/vnpay/vietcombank/login/testwwebview.java
@@ -11,11 +11,13 @@ import commons.Base;
 import commons.PageFactoryManager;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
+import pageObjects.HomePageObject;
 import pageObjects.LogInPageObject;
 
-public class Template extends Base {
+public class testwwebview extends Base {
 	AppiumDriver<MobileElement> driver;
 	private LogInPageObject login;
+	private HomePageObject homePage;
 
 	@Parameters({ "deviceType", "deviceName", "deviceUDID", "hubURL", "appActivities", "appPackage", "appName", "phone", "pass", "otp" })
 	@BeforeClass
@@ -25,12 +27,16 @@ public class Template extends Base {
 		driver = openAndroidApp(deviceType, deviceName, udid, url, appActivities, appPackage, appName);
 		login = PageFactoryManager.getLoginPageObject(driver);
 		login.Global_login(phone, pass, opt);
+		homePage = PageFactoryManager.getHomePageObject(driver);
+		homePage.scrollDownToText(driver, "Mua sắm trực tuyến");
+		homePage.scrollIDownOneTime(driver);
+		homePage.clickToDynamicButtonLinkOrLinkText(driver, "Mua sắm trực tuyến");
+		homePage.waitForElementInvisible(driver, "android.widget.Image[@text='VNShop']");
 
 	}
 
 	@Test
 	public void TC_01_KiemTraChonDiemDenKhiChuaChonDiemDi() {
-
 	}
 
 	@AfterClass(alwaysRun = true)
