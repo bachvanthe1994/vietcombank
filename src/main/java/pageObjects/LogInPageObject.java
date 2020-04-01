ad7377ffa6ec61c035fb3dce5a06599eaa0136dd
Ta Thi Anh <tathianh.3k11@gmail.com>
2020-04-01 10:02:30 +0700

    change login global

--- src/main/java/pageObjects/LogInPageObject.java
+++ src/main/java/pageObjects/LogInPageObject.java
@@ -33,6 +33,7 @@ public class LogInPageObject extends AbstractPage {
 		inputToDynamicOtp(driver, otp, "Tiếp tục");
 		clickToDynamicButton(driver, "Tiếp tục");
 		clickToDynamicAcceptButton("com.android.packageinstaller:id/permission_allow_button");
+		clickToDynamicButtonLinkOrLinkText(driver, "Trang chủ");
 
 		clickToDynamicButtonLinkOrLinkText(driver, "Nhấn giữ để di chuyển nhanh đến các nhóm chức năng");
 
