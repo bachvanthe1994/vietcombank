351336018a27a5ad2e356311bd38ea3b52b5ab71
Ta Thi Anh <tathianh.3k11@gmail.com>
2020-04-10 16:44:08 +0700

    fix register Online

--- src/test/java/vnpay/vietcombank/register_online/Flow_RegisterOnline_LoginUser_Part2.java
+++ src/test/java/vnpay/vietcombank/register_online/Flow_RegisterOnline_LoginUser_Part2.java
@@ -37,14 +37,15 @@ public class Flow_RegisterOnline_LoginUser_Part2 extends Base {
 		
 		log.info("Before class: Lay ten user");
 		nameCustomer = registerOnline.getDynamicTextDetailByIDOrPopup(driver, "com.VCB:id/tvFullname");
+		
+		log.info("TC_01_Step: Click chuc nang dang ky truc tuyen");
+		registerOnline.clickToDynamicButtonLinkOrLinkText(driver, "Đăng ký trực tuyến");
 	}
 
 //	@Test
 	public void TC_01_DangKyDichVuNganHangDienTu_ManHinhDangKyDVNHDT() {
 
 
-		log.info("TC_01_Step: Click chuc nang dang ky truc tuyen");
-		registerOnline.clickToDynamicButtonLinkOrLinkText(driver, "Đăng ký trực tuyến");
 
 		log.info("TC_01_Step: Click giao dich tien mat/chuyen tien");
 		registerOnline.clickToDynamicButtonLinkOrLinkText(driver, "Đăng ký dịch vụ ngân hàng điện tử/Đăng ký phát hành thẻ ghi nợ");
@@ -358,6 +359,8 @@ public class Flow_RegisterOnline_LoginUser_Part2 extends Base {
 		registerOnline.clickToDynamicButton(driver, "Thực hiện đăng ký khác");
 	}
 
+	
+	
 	@Test
 	public void TC_07_DangKyPhatHanhTheGhiNo_ChonHangChuanVCBConnect24VISATrichNoTuDongTuTK() {
 		log.info("TC_07_Step: Nhap thong tin man hinh dang ky");
