4a9160d6a5fd8bd382ea0c8f44143c6d2234e911
TrangVuThy <47878840+TrangVuThy@users.noreply.github.com>
2020-04-28 17:03:19 +0700

    update thanh toán tiền điện

--- src/test/java/vietcombank_test_data/Electric_Bills_Data.java
+++ src/test/java/vietcombank_test_data/Electric_Bills_Data.java
@@ -9,7 +9,9 @@ public class Electric_Bills_Data {
 
 		public static final String ELECTIC_BILL_TITLE = "Hóa đơn tiền điện";
 		public static final String VERIFY_INFO_TITLE_HEAD = "Quý khách vui lòng kiểm tra thông tin giao dịch đã khởi tạo";
-		public static final String ELECTRIC_BILL_MESSAGE = "Giao dịch không thành công do hóa đơn không còn nợ cước. Quý khách vui lòng kiểm tra lại.";
+		public static final String ELECTRIC_BILL_MESSAGE = "Hóa đơn không còn nợ cước. Quý khách vui lòng kiểm tra lại!";
+		
+		
 
 	}
 
@@ -21,8 +23,18 @@ public class Electric_Bills_Data {
 		public static final String EVN_MIEN_BAC = "EVN Miền Bắc";
 		public static final String EVN_MIEN_TRUNG = "EVN Miền Trung";
 		public static final String EVN_MIEN_NAM = "EVN Miền Nam";
-		public static final List<String> LIST_CUSTOMER_ID = Arrays.asList("0988781905", "0989735001", "0980080904",
-				"0860080906", "0980080905", "0860080907", "0980080906", "0860080908", "0980080999", "0860080905");
+		public static final List<String> LIST_CUSTOMER_ID = Arrays.asList( "PP01000105069", "PP03000912871", "PP03000885763", "PP03000931491",
+				"PP03000834139","PP03000804695","PP03000812188","PC05BB0930006","PC05BB0927422","PC05BB0923873","PC05BB0902702","PC05BB0504776",
+				"PC10AA0441972","PC10AA0441712","PC10AA0519494","PC10AA0441822");
 	}
-
 }
+
+
+
+
+
+
+
+
+
+
