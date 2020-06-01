8874f6cd47c472a8323a88c31499a1b1406edffb
HoangMinhKhong <hoangkm197@gmail.com>
2020-06-01 14:27:18 +0700

    add file ReportTransfer_Data.java

---
+++ src/test/java/vietcombank_test_data/ReportTransfer_Data.java
@@ -0,0 +1,14 @@
+package vietcombank_test_data;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ReportTransfer_Data {
+    public class textTite {
+	public static final String TITLE_REPORT = "Báo cáo giao dịch";
+	public static final String CODE_TRANSFER = "Số lệnh giao dịch";
+	public static final String ACCOUNT_CARD = "Tài khoản/thẻ trích nợ";
+	public static final String DETAIL_TRANSFER = "Chi tiết giao dịch";
+	public static final String MONEY_TRANSFER = "Số tiền giao dịch";
+    }
+}
