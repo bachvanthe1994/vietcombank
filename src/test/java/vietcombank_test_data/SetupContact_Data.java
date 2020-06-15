ee60adbf01278c366f4e089ebae8c7d6e88cfed5
TrangVuThy <47878840+TrangVuThy@users.noreply.github.com>
2020-06-15 11:25:25 +0700

    Update code

--- src/test/java/vietcombank_test_data/SetupContact_Data.java
+++ src/test/java/vietcombank_test_data/SetupContact_Data.java
@@ -4,6 +4,7 @@ public class SetupContact_Data {
 
 	public static class UI {
 		public static final String[] TYPE_TRANFER = { "Chuyển tiền trong Vietcombank", "Chuyển tiền tới NH khác qua TK", "Chuyển tiền nhanh qua số thẻ", "Chuyển nhanh qua số tài khoản", "Chuyển tiền cho người nhận tại quầy" };
+		public static final String[] NAME_CARD = { "Test1", "Test2" };
 
 		public static final String SAVE_CONTACT_LABEL = "Lưu thụ hưởng";
 	}
