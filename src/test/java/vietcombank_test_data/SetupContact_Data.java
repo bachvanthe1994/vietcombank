36fac94a7e58cab69121313053431a649e9428f5
TrangVuThy <47878840+TrangVuThy@users.noreply.github.com>
2020-06-15 11:21:12 +0700

    Cài đăt quản lí danh bạ

--- src/test/java/vietcombank_test_data/SetupContact_Data.java
+++ src/test/java/vietcombank_test_data/SetupContact_Data.java
@@ -4,11 +4,6 @@ public class SetupContact_Data {
 
 	public static class UI {
 		public static final String[] TYPE_TRANFER = { "Chuyển tiền trong Vietcombank", "Chuyển tiền tới NH khác qua TK", "Chuyển tiền nhanh qua số thẻ", "Chuyển nhanh qua số tài khoản", "Chuyển tiền cho người nhận tại quầy" };
-		public static final String[] NAME_CARD = { "Test1", "Test2" };
-		public static final String[] ACCOUNT = { "0129837294", "012983729x" };
-		public static final String ACCOUNT2 = "012983729x";
-		public static final String BANK = "Ngân hàng DAB";
-		public static final String NAME_INVALID = "Testabc";
 
 		public static final String SAVE_CONTACT_LABEL = "Lưu thụ hưởng";
 	}
