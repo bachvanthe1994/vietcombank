ed3270ad1b5260ade8f7ae8dc647b5d1779ad8a1
DucDM1 <ducdm1@vnpay.com.vn>
2020-06-15 21:54:00 +0700

    fix gobal login, trạng thái chuuyên tien

--- src/test/java/vietcombank_test_data/TransferMoneyInVCB_Data.java
+++ src/test/java/vietcombank_test_data/TransferMoneyInVCB_Data.java
@@ -218,7 +218,7 @@ public class TransferMoneyInVCB_Data {
 		public static final String DETAIL_TRANSACTION = "Chi tiết giao dịch";
 		public static final String CONTENT_TRANSACTION = "Nội dung giao dịch";
 		public static final String TYPE_TRANSACTION = "Loại giao dịch";
-		public static final String STATUS_TRANSACTION = "Trạng thái lệnh chuyển tiền";
+		public static final String STATUS_TRANSACTION = "Trạng thái chuyển tiền";
 		public static final String AMOUNT_CHANGE = "Số tiền quy đổi";
 		public static final String BILL_PAYMENT = "Hóa đơn thanh toán";
 		public static final String DELETE_CONTACT = "Xóa danh bạ thành công";
