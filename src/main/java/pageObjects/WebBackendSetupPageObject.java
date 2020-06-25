00adfd837da1c2c3fa38b39499df3b3cfb9e4e02
Automation Test team <anhtt@vnpay.vn>
2020-06-25 14:10:03 +0700

    Delete login page object

--- src/main/java/pageObjects/WebBackendSetupPageObject.java
+++ src/main/java/pageObjects/WebBackendSetupPageObject.java
@@ -2,10 +2,9 @@ package pageObjects;
 
 import org.openqa.selenium.WebDriver;
 
-import commons.AbstractPage;
 import commons.WebAbstractPage;
 
-public class WebBackendSetupPageObject extends AbstractPage {
+public class WebBackendSetupPageObject extends WebAbstractPage {
 	private WebDriver driver1;
 	WebAbstractPage test = new WebAbstractPage();
 	int longTime = 40;
@@ -14,6 +13,22 @@ public class WebBackendSetupPageObject extends AbstractPage {
 		driver1 = mappingDriver;
 	}
 
+	public void Login_Web_Backend(String username, String passWeb) {
+		inputIntoInputByID(driver1, username, "login-username");
+		inputIntoInputByID(driver1, passWeb, "login-password");
+		clickToDynamicButtonByID(driver1, "btn-login");
+	}
 
-	
+	public void Setup_Assign_Services_Limit() {
+
+	}
+
+	public void Setup_Assign_Services_Type_Limit() {
+
+	}
+
+	public void Setup_Package_Total_Limit() {
+
+	}
+
 }
